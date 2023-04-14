75251e79a58e46356eb011aab34e196406c45051
victorbillaud <victorbillaud@gmail.com>
2023-04-14 17:18:58 +0200

    Prisma link to backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,25 +1,12 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'next/core-web-vitals',
     'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['prettier'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'warn',
   },
 };

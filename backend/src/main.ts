97ff89ad558adaea8e05c17f792a1e27d612e0e2
victorbillaud <victorbillaud@gmail.com>
2023-06-04 16:45:15 +0200

    password set for listen mqtt

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,21 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
-// export class SocketAdapter extends IoAdapter {
-//   createIOServer(
-//     port: number,
-//     options?: ServerOptions & {
-//       namespace?: string;
-//       server?: any;
-//     },
-//   ) {
-//     const server = super.createIOServer(port, {
-//       ...options,
-//     });
-//     return server;
-//   }
-// }
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -24,6 +9,8 @@ async function bootstrap() {
     transport: Transport.MQTT,
     options: {
       url: 'mqtt://mosquitto:1883',
+      username: 'console',
+      password: 'console',
     },
   });
 
